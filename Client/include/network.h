c88b4afc1b76919590f6d48006543e893d1f841c
harsha613 <harsha@Harsha.localdomain>
2026-07-15 13:40:40 +0000

    network header file first commit

---
+++ Client/include/network.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "../common/protocol.h"
+
+namespace sync {
+
+    class Network {
+    public:
+
+    Network();
+    ~Network();
+
+    Network(const Network&) = delete;
+    Network& operator=(const Network&) = delete;
+
+    bool connect(const std::string& ip, uint16_t port);
+
+    void disconnect();
+
+    bool isconnected() const;
+
+    bool sendPacket(Command command, const std::vector<uint8_t>& payload);
+
+    bool receivePacket(Command& command, std::vector<uint8_t>& payload);
+
+    private:
+        bool sendAll(const uint8_t* data, size_t size);
+        bool receiveAll(uint8_t* data, size_t size);
+
+    private:
+        int socketfd;
+        bool connected;
+    };
+}
