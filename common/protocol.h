af79dd65aab4c70df1964637c19cdc1fc493f74e
harsha613 <harsha@Harsha.localdomain>
2026-07-15 15:37:37 +0000

    added htonll and ntohll functions

--- common/protocol.h
+++ common/protocol.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <arpa/inet.h>
 #include <cstdint>
 
 namespace sync {
+
+    inline uint64_t htonll(uint64_t value) {
+        #if __BYTE_ORDER == __ORDER_LITTLE_ENDIAN__
+            return (((uint64_t)htonl(value & 0xFFFFFFFF)) << 32) | htonl(value >> 32);
+        #else
+            return value;
+        #endif
+    }
+
+    inline uint64_t ntohll(uint64_t value) {
+        #if __BYTE_ORDER == __ORDER_LITTLE_ENDIAN__
+            return (((uint64_t)ntohl(value & 0xFFFFFFFF)) << 32) | ntohl(value >> 32);
+        #else
+            return value;
+        #endif
+    }
+
     enum class Command : uint16_t
     {
         LOGIN = 1,
